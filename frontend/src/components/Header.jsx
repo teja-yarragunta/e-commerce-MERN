1b9a1078d3dab52530bfe421ae8ec88eed595ae5
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-02-13 11:20:51 +0530

    added react routes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,27 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart,FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
-const header = () => {
+const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
-                <img src={logo} alt='OnePieceShop' />
-                OnePiece Shop
-                </Navbar.Brand>
+            <Navbar.Brand as={Link} to='/'>
+            <img src={logo} alt='ProShop' />
+            ProShop
+          </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                    <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                    <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
+                   
+                    <Nav.Link as={Link} to='/cart'><FaShoppingCart /> Cart</Nav.Link>
+                    
+                    <Nav.Link as={Link} to='/login'>
+                  <FaUser /> Sign In
+                </Nav.Link>
+                    
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -24,4 +30,4 @@ const header = () => {
   )
 }
 
-export default header
+export default Header
