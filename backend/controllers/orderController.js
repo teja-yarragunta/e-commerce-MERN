0eee4ff56d4dd1ecb2d039b146c119caf5bfa48c
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-02-20 23:09:47 +0530

    order routes & controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,54 @@
+import exp from 'constants';
+import asyncHandler from '../middleware/asyncHandler.js';
+import Order from '../models/orderModel.js';
+
+// desc: Create new order
+// route: POST /api/orders
+// access: Private
+const addOrderItems = asyncHandler(async(req, res) => {
+    res.send('add order items')
+});
+
+// desc: get logged in users orders
+// route: GET /api/orders/myorders
+// access: Private
+const getMyOrders = asyncHandler(async(req, res) => {
+    res.send('get my orders')
+});
+
+// desc: Get order by id
+// route: GET /api/orders/:id
+// access: Private
+const getOrderById = asyncHandler(async(req, res) => {
+    res.send('get order by id')
+});
+
+// desc: Update order to paid
+// route: GET /api/orders/:id/pay
+// access: Private
+const updateOrderToPaid = asyncHandler(async(req, res) => {
+    res.send('update order to paid')
+});
+
+// desc: update order to delivered
+// route: POST /api/orders/:id/deliver
+// access: Private/Admin
+const updateOrderToDelivered = asyncHandler(async(req, res) => {
+    res.send('update order to delivered')
+});
+
+// desc: get all orders
+// route: GET /api/orders/
+// access: Private/Admin
+const getOrders = asyncHandler(async(req, res) => {
+    res.send('get all orders')
+});
+
+export {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders
+} 
