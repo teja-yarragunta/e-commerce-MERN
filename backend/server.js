c8377ae414024d1ef55f52815cef274de207e5db
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-02-20 17:12:10 +0530

    payment method

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 connectDB();
 
