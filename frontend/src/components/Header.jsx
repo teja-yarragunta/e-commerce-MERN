7e65fdb3c528d0f31cf8df13766887238a7e9476
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-02-20 10:49:23 +0530

    login functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart,FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import {useSelector} from 'react-redux';
@@ -6,6 +6,10 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
   const {cartItems} = useSelector((state) => state.cart);
+  const {userInfo} = useSelector((state) => state.auth);
+  const logoutHandler = () => {
+    console.log('logout')
+  }
 
   return (
     <header>
@@ -22,19 +26,29 @@ const Header = () => {
                     <Nav.Link as={Link} to='/cart'><FaShoppingCart /> Cart
                     {
                       cartItems.length > 0 && (
-                        <Badge pill bg='success' style={{marginLeft: '5px'}}>
-                          
+                        <Badge pill bg='success' style={{marginLeft: '5px'}}>                         
                           {cartItems.reduce((a, c) => a + c.qty, 0)}
-                        
                         </Badge>
                       )
                     }
-                    
                     </Nav.Link>
                     
-                    <Nav.Link as={Link} to='/login'>
+                    {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <NavDropdown.Item as={Link} to='/profile'>
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <Nav.Link as={Link} to='/login'>
                   <FaUser /> Sign In
                 </Nav.Link>
+              )}
                     
                     </Nav>
                 </Navbar.Collapse>
