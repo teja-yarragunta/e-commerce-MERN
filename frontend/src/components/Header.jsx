c28e701213b7d43991d5a0fbad6c47c052964a3e
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-02-20 11:29:03 +0530

    register functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,28 @@
+import {useNavigate} from 'react-router-dom'
 import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart,FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import {logout} from '../slices/authSlice'
 import logo from '../assets/logo.png';
+
 
 const Header = () => {
   const {cartItems} = useSelector((state) => state.cart);
   const {userInfo} = useSelector((state) => state.auth);
-  const logoutHandler = () => {
-    console.log('logout')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async() => {
+    try {
+      await logoutApiCall().unwrap()
+      dispatch(logout())
+      navigate('/login')
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
