390d54c07ee89405f16bd34b4aa1b17a4d6bd9aa
Teja Yarragunta <tejayarragunta@Tejas-MacBook-Air.local>
2025-02-28 12:24:13 +0530

    order screen init

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { Col, Row } from 'react-bootstrap'
 
 const OrderScreen = () => {
+  const { id: orderId } = useParams()
+  const { data: order, refetch, isLoading, error } = useGetOrderDetailsQuery(orderId)
   return (
-    <div>OrderScreen</div>
+    isLoading ? <Loader /> : error ? <Message varient="danger" /> : (
+      <>
+        <h1>Order: {order._id}</h1>
+        <Row>
+          <Col md={8}>Column</Col>
+          <Col md={4}>Column</Col>
+        </Row>
+      </>
+    )
   )
 }
 
